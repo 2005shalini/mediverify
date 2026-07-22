f15439eccf4ed2a3e801bf69446c5095d8b89a6c
2005shalini <shalinirichhariya59@gmail.com>
2026-07-22 21:52:16 +0530

    Setup Flask backend configuration

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "message": "Welcome to MediVerify Backend 🚀"
+    }
+
+if __name__ == "__main__":
+   app.run(host="127.0.0.1", port=5001, debug=True)
